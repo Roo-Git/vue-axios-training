32893574cae3f8849c30ae7360546eb847f67fc0
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-05 19:55:43 +0200

    Created get method

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -18,12 +20,39 @@
 </template>
 
 <script>
+const surveysUrl =
+  "https://vue-axios-backend-default-rtdb.firebaseio.com/surveys.json";
 import SurveyResult from "@/components/survey/SurveyResult.vue";
 
 export default {
-  props: ["results"],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(surveysUrl)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          const dataResults = [];
+          for (const id in data) {
+            dataResults.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = dataResults;
+        });
+    },
   },
 };
 </script>
