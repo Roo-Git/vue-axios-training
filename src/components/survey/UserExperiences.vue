59479e143eae6e33f5bc3de5ded19adb419a0e48
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-05 20:00:59 +0200

    showing loading message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -31,10 +32,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(surveysUrl)
         .then((response) => {
           if (response.ok) {
@@ -42,6 +45,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const dataResults = [];
           for (const id in data) {
             dataResults.push({
